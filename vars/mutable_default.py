bbf5fa31e0d1bc780d84920e4118c2f21e900193
harleylara <harley.lara@outlook.com>
2024-09-28 15:38:40 +0200

    add example

---
+++ vars/mutable_default.py
@@ -0,0 +1,23 @@
+def add_to_defult(my_list=[]):
+    my_list.append(4)
+    return my_list.append(4)
+
+def add_to_defult_return(my_list=[]):
+    return my_list.append(4)
+
+first_call = add_to_defult()
+print(f"first_call (add_to_defult): {first_call = }")
+
+second_call = add_to_defult()
+print(f"second_call (add_to_defult): {second_call = }")
+
+first_call_return = add_to_defult_return()
+print(f"first_call (add_to_defult_return): {first_call_return = }")
+
+second_call_return = add_to_defult()
+print(f"second_call (add_to_defult_return): {second_call_return = }")
+
+def mutable_default(my_list=None):
+    my_list = my_list or []
+    my_list.append(4)
+    return my_list
