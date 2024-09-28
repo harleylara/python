8f84144f553015dd2e79f64c45c6d8c166805574
harleylara <harley.lara@outlook.com>
2024-09-28 13:54:50 +0200

    expand example

--- vars.py
+++ vars.py
@@ -7,3 +7,16 @@ print(hex(id(x)))
 # we are pointing to a different memory location
 x = 999
 print(hex(id(x)))
+
+# in this case both share the same address
+y = x
+print(f"x addr: {hex(id(x))}, value: {x = }")
+print(f"y addr: {hex(id(y))}, value: {y = }")
+
+# but if you change one
+# you are creating a new PyObject
+# and python update the memory location of 
+# that variable
+x = 6
+print(f"x addr: {hex(id(x))}, value: {x = }")
+print(f"y addr: {hex(id(y))}, value: {y = }")
